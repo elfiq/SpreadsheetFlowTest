1a3fb0f10fb429b33a35f27852fdbd169303bf3a
Rodion <trofimov@playform.com>
2014-11-22 14:19:54 +0300

    drag slots

--- app/nodeboard/node/node.js
+++ app/nodeboard/node/node.js
@@ -7,7 +7,6 @@ angular.module('SpreadsheedFlow.Nodeboard')
         return {
             compile:function(element, attrs) {
                 var linkFunction = function(scope, element, attrs) {
-                    // Title element
                     var $el = $(element);
                     var startDragPoint = {x:0,y:0};
                     interact($el[0])
@@ -30,6 +29,8 @@ angular.module('SpreadsheedFlow.Nodeboard')
                             resistance : 20
                         });
 
+
+
                     scope.$watch("node", function (value) {
                         setTimeout(function () {
                             var title = $el.find(".title");
@@ -42,7 +43,10 @@ angular.module('SpreadsheedFlow.Nodeboard')
                             var cnt = ingoingSlots.length;
                             var i;
                             for (i=0;i<cnt;i++) {
-                                ingoingSlots.eq(i).attr('transform',"translate("+(i*25-(cnt-1)/2*25)+","+(-h/2-7)+")");
+                                ingoingSlots.eq(i).scope().position = {x:(i*25-(cnt-1)/2*25), y:(-h/2-7)};
+                                ingoingSlots.eq(i).scope().$apply();
+
+                                //ingoingSlots.eq(i).attr('transform',"translate("+(i*25-(cnt-1)/2*25)+","+(-h/2-7)+")");
                             }
                             var outgoingSlots = $el.find(".outgoing");
                             cnt = outgoingSlots.length;
@@ -56,6 +60,55 @@ angular.module('SpreadsheedFlow.Nodeboard')
                 return linkFunction;
             }
         };
+    }])
+    .directive("sfIngoingSlot", ['$timeout', function ($timeout) {
+        console.log('ererer');
+        return {
+            compile: function (element, attrs) {
+                console.log('erere');
+                return function (scope, element, attrs) {
+                    var $el = $(element);
+
+                    var disconnect = function () {
+
+                    }
+                    var startPos = {x:0,y:0};
+                    interact($el[0])
+                        .draggable({
+                            onstart:function () {
+                                console.log('start');
+                                startPos.x = scope.position.x;
+                                startPos.y = scope.position.y;
+                            },
+                            onmove:function (event) {
+                                console.log(event);
+                                var distance = scope.$parent.convertDistanceClientToBoard(event.clientX - event.clientX0,event.clientY - event.clientY0);
+                                scope.position.x = startPos.x + distance.x/3;
+                                scope.position.y = startPos.y + distance.y/3;
+                                scope.$apply();
+                                if (Math.abs(scope.position.x - startPos.x) + Math.abs(scope.position.y - startPos.y)<10) {
+
+                                }
+                                if (event.duration>500) {
+                                    event.interaction.stop(event);
+                                    scope.position.x = startPos.x;
+                                    scope.position.y = startPos.y;
+                                    scope.$apply();
+                                }
+                            },
+                            onend: function (event) {
+                                if (Math.abs(scope.position.x - startPos.x) + Math.abs(scope.position.y - startPos.y)<10) {
+
+                                }
+                                scope.position.x = startPos.x;
+                                scope.position.y = startPos.y;
+                                scope.$apply();
+                            }
+                        })
+
+                }
+            }
+        }
     }])
     .animation(".show-node", function () {
         return {
